1c0f5ea6c502d567ed21c20831bc14650902a766
_asdx_ <xasdx@tuta.io>
2017-12-12 15:43:58 +0000

    Implement GET articles/:id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
   end
+  
   def create
     @article = Article.new(article_params)
     @article.save
