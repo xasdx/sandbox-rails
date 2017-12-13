80d3957b0393e489084d783c599f8021f4400c9d
_asdx_ <xasdx@tuta.io>
2017-12-13 16:10:58 +0000

    Add validation to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   
   def index
-    @article = Article.all
+    @articles = Article.all
   end
   
   def show
@@ -9,12 +9,16 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
   end
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   
   private
