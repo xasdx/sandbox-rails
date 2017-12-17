3772755193fd3a904a5969263ea40cbb8a30d33a
_asdx_ <xasdx@tuta.io>
2017-12-17 11:23:12 +0000

    Add basic http auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  http_basic_authenticate_with name: "asd", password: "pwd", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
